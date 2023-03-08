4fa779b82a2fe7bb48b1282ce7e87b2068221b6b
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 09:56:04 +0530

    ADDED_UC3_ADDEDPARTTIME

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -3,23 +3,29 @@ package com.employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
-        System.out.println(" Check whether Employee is present or absent");
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_WAGE_PER_HOUR = 20;
 
         int empHrs, empWage;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == IS_PRESENT){
-            System.out.println("Employee is Present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Doing FUll Time.");
             empHrs = 8;
         }
+
+        else if (empCheck == IS_PART_TIME) {
+            System.out.println("Employee is Doing Part Time.");
+            empHrs = 4;
+        }
         else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent.");
             empHrs = 0;
         }
-        //calculation
+
         empWage = empHrs * EMP_WAGE_PER_HOUR;
         System.out.println("Emp Hours : "+empHrs);
         System.out.println("Emp Wage : "+empWage);
+
     }
 }
