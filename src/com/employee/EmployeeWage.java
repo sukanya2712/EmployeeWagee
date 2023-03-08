baa8768d0e99538e7f0db51aaeda7f76e8617329
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 10:20:49 +0530

    ADDED_UC7_REFRACTOECODEUSINGMETHODS

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -7,10 +7,8 @@ public class EmployeeWage {
     private static final int EMP_WAGE_PER_HOUR = 20;
     private static final int WORKING_DAYS_PER_MONTH = 20;
     private static final int TOTAL_WORKING_HOURS = 100;
-    public static void main(String[] args){
 
-        System.out.println(" Welcome to employee wage");
-
+    public static void wagesofEmployees(){
         int empHrs=0, empWagePerMonth = 0, totalWorkingDays=0, totalEmpHours = 0;
 
         while(totalEmpHours <= TOTAL_WORKING_HOURS && totalWorkingDays < WORKING_DAYS_PER_MONTH) {
@@ -41,8 +39,10 @@ public class EmployeeWage {
         }
 
         System.out.println("Emp Wage Per Month : "+empWagePerMonth);
-
-
+    }
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation program");
+        wagesofEmployees();
         }
     }
 
