dfee0dc7d0376f33e2e9fec7647c6247d14e766c
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 09:45:29 +0530

    Welcome_to_emp_wage_computation

---
+++ src/com/employee/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.employee;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
+    }
+}
