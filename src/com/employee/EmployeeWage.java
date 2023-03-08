f77460cbbf046bc79d89c0430f705330aae0253a
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 10:06:55 +0530

    ADDED_UC4_USINGSWITCHCASE

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -1,38 +1,37 @@
 package com.employee;
 
 public class EmployeeWage {
+
+    private static final int IS_FULL_TIME = 1;
+    private static final int IS_PART_TIME = 2;
+    private static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
-        private static final int IS_FULL_TIME = 1;
-        private static final int IS_PART_TIME = 2;
-        private static final int EMP_WAGE_PER_HOUR = 20;
 
-        public static void main(String[] args) {
+        System.out.println(" Using switch case ");
 
-            System.out.println(" Using switch case ");
+        int empHrs, empWage;
+        int empType = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("Welcome to Employee Wage Computation");
 
-            int empHrs, empWage;
-            int empType = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println("Welcome to Employee Wage Computation");
+        switch (empType) {
+            case IS_FULL_TIME:
+                System.out.println("Employee is Doing Full Time:");
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Employee is Doing Part Time:");
+                empHrs = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent:");
+                empHrs = 0;
+                break;
+        }
 
-            switch (empType) {
-                case IS_FULL_TIME:
-                    System.out.println("Employee is Doing Full Time:");
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    System.out.println("Employee is Doing Part Time:");
-                    empHrs = 4;
-                    break;
-                default:
-                    System.out.println("Employee is Absent:");
-                    empHrs = 0;
-                    break;
-            }
-
-            empWage = empHrs * EMP_WAGE_PER_HOUR;
-            System.out.println("Emp Hours : " + empHrs);
-            System.out.println("Emp Wage : " + empWage);
-
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : " + empHrs);
+        System.out.println("Emp Wage : " + empWage);
+        }
     }
-}
+
