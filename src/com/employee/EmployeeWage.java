98b770e7c1c8601ca247214dc81c641461cda853
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 09:48:06 +0530

    ADDED_UC1_TOCHECKATTENDENCE

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -3,5 +3,14 @@ package com.employee;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
+        System.out.println(" Check whether Employee is present or absent");
+        int empPresesent = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == empPresesent){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
