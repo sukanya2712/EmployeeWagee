144caa25abb33ede23312eef44c4a4a41040b8c7
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-08 09:53:04 +0530

    ADDED_UC2_TOCALDAILYWAGE

--- src/com/employee/EmployeeWage.java
+++ src/com/employee/EmployeeWage.java
@@ -4,13 +4,22 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAMME");
         System.out.println(" Check whether Employee is present or absent");
-        int empPresesent = 1;
+        int IS_PRESENT = 1;
+        int EMP_WAGE_PER_HOUR = 20;
+
+        int empHrs, empWage;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if(empCheck == empPresesent){
+        if(empCheck == IS_PRESENT){
             System.out.println("Employee is Present");
+            empHrs = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
         }
+        //calculation
+        empWage = empHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Emp Hours : "+empHrs);
+        System.out.println("Emp Wage : "+empWage);
     }
 }
